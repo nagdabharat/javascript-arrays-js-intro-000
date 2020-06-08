f0ad396fc405e9ffc3ea9e982af77f7381763866
Bharat Nagda <nagdabharat@gmail.com>
2020-06-08 13:04:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array = array.slice(0,array.length - 1);
+  return array.slice(0,array.length - 1);
 }
