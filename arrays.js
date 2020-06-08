9381bb1f0eb46ac8644035ff45b5f7976d90cee3
Bharat Nagda <nagdabharat@gmail.com>
2020-06-08 12:38:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 // chocolateBars.push("milky way")
 function addElementToBeginningOfArray(array, element) {
-  array = [element, ...array]
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element,...array]
+  return array
 }
