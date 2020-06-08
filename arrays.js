a319410522d0ed2214c0c66ac27ad6f44ced947d
Bharat Nagda <nagdabharat@gmail.com>
2020-06-08 12:35:59 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+// chocolateBars.push("milky way")
+function addElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+}
