62bc900420b53f74bf4854364a98ca8a28d0a62e
Bharat Nagda <nagdabharat@gmail.com>
2020-06-08 12:58:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.unshift();
-  return array;
+  array;
 }
 
 function removeElementFromBeginningOfArray(array) {
